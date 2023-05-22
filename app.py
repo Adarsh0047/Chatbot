ce1f05bee488ca303a92902c716a73620fa6e4a8
Adarsh <37295203+Adarsh0047@users.noreply.github.com>
2023-05-22 10:12:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
-from streamlit_extras.add_vertical_space import add_vertical_space
 st.set_page_config(page_title="Chatbot")
 
 st.title("Chatbot")
