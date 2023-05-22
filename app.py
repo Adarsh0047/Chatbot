7b26d462818a4234de67b569f1fc559fb1f01897
Adarsh <37295203+Adarsh0047@users.noreply.github.com>
2023-05-22 10:08:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 st.set_page_config(page_title="Chatbot")
+
 st.title("Chatbot")
 if "generated" not in st.session_state:
     st.session_state["generated"] = ["Hello! Welcome"]
@@ -15,8 +16,11 @@ colored_header(label='', description='', color_name='blue-30')
 response_container = st.container()
 
 def get_text():
-    input_text = st.text_input("You: ", "", key="input")
-    return input_text
+    with st.form("my_form", clear_on_submit=True):
+        input_text = st.text_input("You: ", "", key="input")
+        submitted = st.form_submit_button("Go")
+        if submitted:
+            return input_text
 with input_container:
     user_input = get_text()
 
@@ -34,4 +38,3 @@ with response_container:
         for i in range(len(st.session_state["generated"])):
             message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
             message(st.session_state["generated"][i], key=str(i) + "_bot")
-
